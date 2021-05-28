c733600aad11a8c17270b48a242be1e5835bebd8
Ricky Arora <your_email_here>
2021-05-28 18:21:47 -0400

    iOS apps

---
+++ Documents/iOS_app/MyPlayground.playground/Contents.swift
@@ -0,0 +1,17 @@
+var population = 9_000_000
+print(population)
+
+var str1 = """
+This string
+is multiple lines
+so cool.
+Bruh!
+"""
+
+var v2 """
+HI \
+my name is: Ricky \
+sep line
+yeah
+boy
+"""
